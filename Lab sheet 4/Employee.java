d44d4b86c8f62ca7e68162fd7f5e8167cbe0f948
Nicko51 <gihansss123@gmail.com>
2023-07-31 17:51:00 +0530

    Done

---
+++ Lab sheet 4/Employee.java
@@ -0,0 +1,34 @@
+
+package com.mycompany.lab_4;
+
+public class Employee
+{
+    
+    private int empId;
+    private String empName;
+    private String empDesignation;
+    public void setEmpId(int empId)
+    {
+        this.empId=empId;
+    }
+    public int getEmpId()
+    {
+        return empId;
+    }
+    public void setEmpName(String empName)
+    {
+        this.empName=empName;
+    }
+    public String getEmpName()
+    {
+        return empName;
+    }
+    public void setEmpDesignation(String empDesignation)
+    {
+        this.empDesignation=empDesignation;
+    }
+    public String getEmpDesignation()
+    {
+        return empDesignation;
+    }
+}
